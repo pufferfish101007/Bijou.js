b9cfeac70bc1b41fcadfc405e55a9e32c32f2ff7
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2021-04-15 13:18:07 +0100

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
-const groups = ["array", "color", "math", "function", "element", "object", "event", "string", "utility"];
+const groups = ["array", "color", "math", "function", "element", "object", "event", "string", "utility", "date"];
 
 groups.forEach(group => {
   let js = fs.readFileSync(`./js/${group}.js`, { encoding: "utf8" });
+  const jsdocRe = /\/\*\*\s*\n([^\*]|(\*(?!\/)))*\*\//gm
+  const jsdocs = js.match(jsdocRe);
   
 });
